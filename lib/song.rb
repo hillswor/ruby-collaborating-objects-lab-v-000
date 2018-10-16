66e1b3255a6ff7dd861d7ff9cd91887c0d675fb3
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-16 16:40:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,21 @@
-require "pry"
-
 class Song
 
-  attr_accessor :name, :artist
+  attr_accessor :artist, :name
 
   def initialize(name)
     @name = name
   end
 
-  def artist(name)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
 
-  def self.new_by_filename(filename)
-    artist_and_song = filename.chomp(".mp3").split(" - ")
-    self.new(artist_and_song[1])
-    self.artist(artist_and_song[0])
-
-
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    self = self.new(song_info[1])
+    self.artist_name = song_info[0]
+    self
   end
 
 end
