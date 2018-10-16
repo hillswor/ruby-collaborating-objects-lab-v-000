127227d1e2d1685ed02d578e1ab7a6c0118bff04
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-16 16:45:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 class Artist
 
   attr_accessor :name, :songs
@@ -24,17 +22,19 @@ class Artist
   end
 
   def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
+    self.all.detect {|artist| artist.name == name}
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else self.create(name)
     end
   end
 
-  def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || self.new(name)
-  end
-
-  def print_songs
-    puts @songs.collect {|song| song.name}
-  end
-
-end
+    
