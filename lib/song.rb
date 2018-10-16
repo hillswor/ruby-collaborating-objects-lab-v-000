954d8753c4c47dda84757957b442c9a364c47230
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-16 16:33:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,7 @@ class Song
   def self.new_by_filename(filename)
     artist_and_song = filename.chomp(".mp3").split(" - ")
     self.new(artist_and_song[1])
+    self.artist(artist_and_song[0])
 
 
   end
